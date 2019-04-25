9bbd59759524ccd32550695e5984f513ba09dda1
ipalakot <ipalakot@gmail.com>
2019-04-25 08:39:00 +0200

    maj autoload

---
+++ rasef_last/src/Rasef/GenderBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rasef\GenderBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('RasefGenderBundle:Default:index.html.twig');
+    }
+}
---
+++ rasef_last/src/Rasef/GenderBundle/RasefGenderBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rasef\GenderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RasefGenderBundle extends Bundle
+{
+}
